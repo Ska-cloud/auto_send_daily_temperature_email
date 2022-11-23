615a9bc873382fe5549ac3c8a2e26b7820f40004
ska_0120 <73944365+Ska-cloud@users.noreply.github.com>
2022-11-23 16:28:39 +0800

    Update crontab.py

--- decorator_func/crontab.py
+++ decorator_func/crontab.py
@@ -6,8 +6,10 @@ import schedule
 def crontab(timestamp):
 
     def _crontab(func):
+        
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            
             # 指定定时器的执行周期
             schedule.every().day.at(timestamp).do(func, *args, **kwargs)
 
